6752ec4d65eb0b576899e584f47d0ae8f71187a0
vinayak shinde <vinayak.shinde.058@gmail.com>
2023-06-03 22:37:27 +0530

    Two microservices communicate with each other code

---
+++ customer/src/main/java/com/amigoscode/customer/Customer.java
@@ -0,0 +1,34 @@
+package com.amigoscode.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="customer")
+public class Customer {
+
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+    private Integer id;
+    @Column(name = "first_Name")
+    private String firstName;
+    @Column(name = "last_Name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+}
---
+++ customer/src/main/java/com/amigoscode/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,6 @@
+package com.amigoscode.customer;
+
+public record CustomerRegistrationRequest(String firstName,
+                                          String lastName,
+                                          String email) {
+}
